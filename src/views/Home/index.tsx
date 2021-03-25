8f3287faf202a476cf0377833b7d72cd9d52972c
burczan <natalia.wajnor@gmail.com>
2021-03-25 13:58:22 +0100

    add Home view

---
+++ src/views/Home/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Hero } from '../../common/components/Hero';
+
+export const Home = () => {
+  return (
+    <div>
+      <Hero
+        title="Morse Academy"
+        subtitle="Time to learn Morse Code!"
+        color="is-primary"
+        size="is-large"
+      />
+    </div>
+  );
+};
